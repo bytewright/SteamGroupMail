03d0a8013311f2c8f3d880104355d818032233a7
Bytewright <bytewright@t-online.de>
2017-02-20 23:21:08 +0100

    refactor

--- run_service.py
+++ run_service.py
@@ -19,23 +19,23 @@ def get_args():
     configpath = os.path.join(os.path.dirname(__file__), 'config.ini')
     parser = configargparse.ArgParser(default_config_files=[configpath])
     parser.add_argument('--urljson', type=str,
-                        help='json file, gets parsed from trainingsjobs')
+                        help='json file, containing rss-feeds')
     parser.add_argument('--emailjson', type=str,
-                        help='path to caffe home dir')
-    parser.add_argument('--dbaddr', type=str,
-                        help='path to caffe home dir')
+                        help='json file, containing recipients')
     parser.add_argument('--smtpaddr', type=str,
-                        help='Path, where auto_trainer will create an output-directory')
+                        help='address of mail server')
     parser.add_argument('--smtpport', type=int,
-                        help='Path, where auto_trainer will create an output-directory')
+                        help='port of mail server')
     parser.add_argument('--smtpusr', type=str,
-                        help='Path, where auto_trainer will create an output-directory')
+                        help='user login for mail server')
     parser.add_argument('--smtppw', type=str,
-                        help='Path, where auto_trainer will create an output-directory')
+                        help='password for mail server')
+    parser.add_argument('--dbaddr', type=str,
+                        help='address of db')
     parser.add_argument('--dbusr', type=str,
-                        help='Path, where auto_trainer will create an output-directory')
+                        help='user login for db')
     parser.add_argument('--dbpw', type=str,
-                        help='Path, where auto_trainer will create an output-directory')
+                        help='password for db')
     parser.add_argument('--debug', help='Debug Mode', action='store_true')
     #parser.set_defaults(DEBUG=True)
 
@@ -45,22 +45,18 @@ def get_args():
 def send_email(gmail_user, gmail_pwd, recipient, subject, body):
     FROM = gmail_user
     TO = recipient if type(recipient) is list else [recipient]
-    SUBJECT = subject
-    TEXT = body
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    server = smtplib.SMTP("smtp.gmail.com", 587)
+    """ % (FROM, ", ".join(TO), subject, body)
     try:
         server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server_ssl.ehlo() # optional, called by login()
+        server_ssl.ehlo()
         server_ssl.login(gmail_user, gmail_pwd)
-        # ssl server doesn't support or need tls, so don't call server_ssl.starttls()
         server_ssl.sendmail(FROM, TO, message)
         server_ssl.close()
-        print('successfully sent the mail')
+        log.info('successfully sent the mail')
     except:
-        print("failed to send mail")
+        log.error("failed to send mail")
 
 
 def get_rss_tags(url):
@@ -83,17 +79,19 @@ if __name__ == '__main__':
         log.setLevel(logging.DEBUG)
     else:
         log.setLevel(logging.INFO)
-
+    log.info('loading group urls from json...')
     urls_dict = json.load(open(args.urljson, 'r'))
 
-    mail_text = ''
     mail_body = ''
     with open('mail_template.txt', 'r') as f:
         for line in f.readlines():
             mail_body += line
+
+    log.info('loaded mail template:\n'+mail_body)
     for id in urls_dict:
         url = urls_dict[id]['url']
         items = get_rss_tags(url)
+        log.info('got {} announcements from rss feed'.format(items.__len__()))
         for key in items[0]:
             mail_body = mail_body.replace('{'+key+'}', items[0][key])
 
@@ -101,5 +99,5 @@ if __name__ == '__main__':
     recipients = []
     for id in email_dict:
         recipients.append(email_dict[id]['email'])
-    print(mail_body)
+    log.info('sending mail to {} recipients:\n{}'.format(recipients.__len__(), mail_body))
     send_email(args.smtpusr, args.smtppw, recipients, 'TTSS Mail', mail_body)
